5bee96402b4860f145dcacdd74d1ef0db1b0be8e
rishabhkum-Dev-1799 <rishabhkum17@gmail.com>
2023-01-21 22:34:02 +0530

    Adding the Payment Functionality in Client side and Server Side

--- ecommerce-client/src/components/CheckoutForm.js
+++ ecommerce-client/src/components/CheckoutForm.js
@@ -6,16 +6,42 @@ import {
     useStripe,
     useElements,
 } from '@stripe/react-stripe-js';
+import { useCart } from 'react-use-cart';
+import { BACKEND_URL } from '../config';
 
 
 // shippingAddress, city, state, amount, pin, items, token
 const stripePromise = loadStripe('pk_test_51MS0i2SF5PLUhtWZ0yMsefagzhpCmpLmv4ToiJqtWL8sDwT9yz99YwCp80tKNmAMxhz4YZ4Ihki8yfC4BvwMK3sx00GQjJvIMQ');
 const CheckoutFormProvider = () => {
     const [formData, setFromData] = useState({});
+    const [payButton, setPayButton] = useState(true);
+    const [error, setError] = useState(false);
+    const [payProcessing, setPayProcessing] = useState(false);
     const stripe = useStripe();
     const elements = useElements();
+    const { cartTotal, items, emptyCart } = useCart();
 
-    //Functions and handler 
+    /****************************************Functions and handlers Defined  */
+    //Payment Handler 
+    const makePayment = async (allFormData) => {
+        try {
+            const response = await fetch(`${BACKEND_URL}/api/orders`, {
+                method: 'post',
+                headers: {
+                    "Content-type": "application/json",
+                    "Authorization": "Bearer " + localStorage.getItem("jwt"),
+                },
+                body: JSON.stringify(allFormData)
+            });
+            if (response.status != 200) throw Error("Payment Failed");
+            return await response.json();
+        }
+        catch (err) {
+            console.log(err);
+            setError(true);
+        }
+    }
+    //Change handler 
     const changeHandler = (e) => {
         setFromData((prevformData) => {
             return {
@@ -25,11 +51,43 @@ const CheckoutFormProvider = () => {
         })
     }
 
-    const submitHandler = (e) => {
+    //Submit Handler
+    const submitHandler = async (e) => {
         e.preventDefault()
-        console.log(formData);
-    }
+        if (elements == null) {
+            return
+        }
+        const card = elements.getElement(CardElement);
+        const payload = await stripe.createToken(card);
 
+        const allFormData = {
+            ...formData,
+            amount: cartTotal,
+            items: items,
+            token: payload.token.id
+        }
+        console.log(allFormData);
+        setPayProcessing(true);
+        await makePayment(allFormData);
+        setPayProcessing(false);
+        emptyCart();
+
+    }
+    /**JSX Return Statements  */
+    if (payProcessing) {
+        return (
+            <>
+                <h2>Payment is Processing ..........</h2>
+            </>
+        )
+    }
+    if (error) {
+        return (
+            <>
+                <h2 className="red-text"> Payment Failed </h2>
+            </>
+        )
+    }
     return (
         <>
             <div>
@@ -63,9 +121,16 @@ const CheckoutFormProvider = () => {
                         onChange={changeHandler}
                     />
                     <br />
-                    <CardElement />
+                    <CardElement onChange={(e) => {
+                        if (e.complete) {
+                            setPayButton(false);
+                        }
+                        else {
+                            setPayButton(true);
+                        }
+                    }} />
                     <br />
-                    <button type="submit" className="btn #00897b teal darken-1" disabled={!stripe || !elements}>
+                    <button type="submit" className="btn #00897b teal darken-1" disabled={(!stripe || !elements) || payButton}>
                         Pay
                     </button>
                 </form>
--- ecommerce/src/api/order/controllers/order.js
+++ ecommerce/src/api/order/controllers/order.js
@@ -9,14 +9,15 @@ const { createCoreController } = require('@strapi/strapi').factories;
 module.exports = createCoreController('api::order.order', ({ strapi }) => ({
 
     async create(ctx) {
-        const { shippingAddress, city, state, amount, pin, items, token } = ctx.request.body;
+        const { amount, city, items, pin, shippingAddress, state, token } = ctx.request.body;
 
-        await stripe.charges.create({
-            amount: amount * 100,
-            curreny: "USD",
-            source: token,
-            description: `order by user ${ctx.state.user.email}`
-        })
+        /** Logic to be Completed With new Stripe Gateway method */
+        // await stripe.charges.create({
+        //     amount: amount * 100,
+        //     currency: "USD",
+        //     source: token,
+        //     description: `order by user ${ctx.state.user.email}`
+        // })
 
         const order = await strapi.db.query('api::order.order').create({
             data: {
